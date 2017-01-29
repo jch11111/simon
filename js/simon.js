001e5a7668cacde080f56e065ccfc71f53a66f62
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2017-01-29 12:34:50 -0800

    adding tones

--- js/simon.js
+++ js/simon.js
@@ -13,18 +13,36 @@ var
         buttonColor: ['#FF0000', '00E1FF', 'F5F500', '00E100']
     },
     jqueryMap = {
-        colorButtons: []
+        colorButtons: [],
+        buttonSounds: []
     };
     //----------------------- END MODULE SCOPE VARIABLES -----------------------
+
+    function handleMouseDown(button) {
+        $(jqueryMap.colorButtons[button]).css('fill', configMap.buttonColorPressed[button]);
+        playSound(jqueryMap.buttonSounds[button]);
+    }
+
+    function handleMouseUp(button) {
+        $(jqueryMap.colorButtons[button]).css('fill', configMap.buttonColor[button]);
+        jqueryMap.buttonSounds[button].pause();
+    }
 
     function init() {
         $(window).load(function () {
             initializeJqueryMap();
+            initializeSounds();
             setEventHandlers();
         })
     };
 
-    function initializeJqueryMap () {
+    function initializeButtonSound (button) {
+        var buttonSound = document.createElement('audio');
+        buttonSound.setAttribute('src', 'media/sound' + button + '.ogg');
+        jqueryMap.buttonSounds.push(buttonSound);
+    }
+
+    function initializeJqueryMap() {
         var $gameImage = jqueryMap.$gameImage = $(document.getElementById('gameImage').contentDocument);
         jqueryMap.colorButtons.push($($gameImage).find('#colorButton0'));
         jqueryMap.colorButtons.push($($gameImage).find('#colorButton1'));
@@ -32,12 +50,33 @@ var
         jqueryMap.colorButtons.push($($gameImage).find('#colorButton3'));
     }
 
-    function handleMouseDown(button) {
-        $(jqueryMap.colorButtons[button]).css('fill', configMap.buttonColorPressed[button]);
+    function initializeSounds () {
+        for (var button = 0; button < 4; button++) {
+            initializeButtonSound(button);
+        }
+        //var buttonSound = document.createElement('audio');
+        //buttonSound.setAttribute('src', 'media/sound0.ogg');
+        //jqueryMap.buttonSounds.push(buttonSound);
+
+        //buttonSound.loop = false;
+
+        //buttonSound.addEventListener('ended', function () {
+        //    playSound(this);
+        //}, false);
+
+        //buttonSound.addEventListener("timeupdate", function () {
+        //    if ( this.currentTime === this.duration ) {
+        //        playSound (this);
+        //    }
+        //}, false);
+
     }
 
-    function handleMouseUp(button) {
-        $(jqueryMap.colorButtons[button]).css('fill', configMap.buttonColor[button]);
+
+
+    function playSound(buttonSound) {
+        buttonSound.currentTime = 0;
+        buttonSound.play();
     }
 
     function setEventHandlers() {
